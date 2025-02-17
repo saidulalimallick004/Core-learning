92d0aea4a3040094aff8a42ad319de2402a7c694
saidulalimallick004 <saidulalimallick1@gmail.com>
2025-02-17 20:48:48 +0530

    Version 1.0.6

--- home/models.py
+++ home/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class students(models.Model):
+    
+    Name=models.CharField(max_length=100)
+    Age=models.IntegerField(null=True)
+    Email=models.EmailField()
+    Add=models.TextField(null=True,blank=True)
+    
+    
+    
+
+class course(models.Model):
+    CourseId=models.CharField(max_length=5)
+    CourseName=models.CharField(max_length=50)
+    FileNotes=models.FileField(null=True)
+    
+    
+class cars(models.Model):
+    Name=models.CharField(max_length=10)
+    Color=models.TextField()
+    
+    
+    
